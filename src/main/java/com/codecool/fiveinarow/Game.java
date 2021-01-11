3c8ebac878b0bafbf1c341353c6c2246772a4062
Polly <polli.burjan@gmail.com>
2021-01-11 17:42:10 +0100

    Add exercise 1 and 2

--- src/main/java/com/codecool/fiveinarow/Game.java
+++ src/main/java/com/codecool/fiveinarow/Game.java
@@ -1,10 +1,24 @@
 package com.codecool.fiveinarow;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Game implements GameInterface {
 
     private int[][] board;
 
     public Game(int nRows, int nCols) {
+        int[][] board = new int[nRows][nCols];
+        int Counter = 0;
+        for (int[] row : board) {
+            int Counter2 = 0;
+            for (int cell : row) {
+                board[Counter][Counter2] = 0;
+                Counter2++;
+            }
+            Counter++;
+        }
+        setBoard(board);
     }
 
     public int[][] getBoard() {
@@ -16,6 +30,53 @@ public class Game implements GameInterface {
     }
 
     public int[] getMove(int player) {
+        char[] abc = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        String[][] moveList = new String[board.length][board[0].length];
+        int Counter = 0;
+        for (String[] row : moveList) {
+            int Counter2 = 0;
+            for (String cell : row) {
+                moveList[Counter][Counter2] = abc[Counter] + String.valueOf(Counter2 + 1);
+                Counter2++;
+            }
+            Counter++;
+        }
+
+        boolean valid = false;
+        Scanner myObj = new Scanner(System.in);
+        String move;
+        System.out.println("Enter your move: ");
+        move = myObj.nextLine();
+
+        while (!valid) {
+            for (String[] row : moveList) {
+                for (String cell : row) {
+                    if (cell.equals(move)) {
+                        valid = true;
+                    }
+                }
+            }
+            if (!valid) {
+                System.out.println("Invalid input or position is already taken");
+                System.out.println("Enter your move: ");
+                move = myObj.nextLine();
+            }
+        }
+
+        Counter = 0;
+        for (String[] row : moveList) {
+            int Counter2 = 0;
+            for (String cell : row) {
+                if (cell.equals(move)) {
+                    moveList[Counter][Counter2] = "---";
+                }
+                Counter2++;
+            }
+            Counter++;
+        }
+        System.out.println(Arrays.deepToString(moveList));
+
         return null;
     }
 
